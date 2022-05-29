6e118c8df750a3cc1b1a7d82ee69243878f8eed6
minhIT432 <94076706+minhIT432@users.noreply.github.com>
2022-05-30 03:46:40 +0700

    Add files via upload

---
+++ src/projectoop/reCeipt.java
@@ -0,0 +1,14 @@
+
+package projectoop;
+
+import java.text.SimpleDateFormat;
+
+
+public class reCeipt {
+    public String codeOrder;
+    public String idStaff;
+    public String status;
+    public SimpleDateFormat Date;
+    public long totalReceipt;
+
+}
